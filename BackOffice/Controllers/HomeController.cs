cce1a2dc15f9da6991f05f1be802330ad17134bf
Sander Kleijnen <2403114kleijnen@zuyd.nl>
2026-06-04 09:50:28 +0200

    Add complete homepage with dynamic stats, chart, notifications and orders table

--- BackOffice/Controllers/HomeController.cs
+++ BackOffice/Controllers/HomeController.cs
@@ -1,20 +1,42 @@
 using BackOffice.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using DataAccessLayer.Interfaces;
+
 
 namespace BackOffice.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IOrderRepository _orderRepository;
+        private readonly IProductRepository _productRepository;
+        private readonly ICustomerRepository _customerRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            ILogger<HomeController> logger,
+            IOrderRepository orderRepository,
+            IProductRepository productRepository,
+            ICustomerRepository customerRepository)
         {
             _logger = logger;
+            _orderRepository = orderRepository;
+            _productRepository = productRepository;
+            _customerRepository = customerRepository;
         }
 
         public IActionResult Index()
         {
+            var orders = _orderRepository.GetAllOrders().ToList();
+            var products = _productRepository.GetAllProducts().ToList();
+            var customers = _customerRepository.GetAllCustomers().ToList();
+
+            ViewBag.TotalProducts = products.Count;
+            ViewBag.TotalOrders = orders.Count;
+            ViewBag.TotalCustomers = customers.Count;
+            ViewBag.RecentOrders = orders;
+            ViewBag.Products = products;
+
             return View();
         }
 
