c19c04cef71ffec86592264403b0a0e92d079496
BeetleBeast <78571657+BeetleBeast@users.noreply.github.com>
2026-06-05 14:51:17 +0200

    added ViewModel van sem

---
+++ BackOffice/DataAccessLayer/Models/PaginatedProductViewModel.cs
@@ -0,0 +1,20 @@
+﻿
+using DataAccessLayer.Models;
+using System.Collections.Generic;
+
+namespace BackOffice.DataAccessLayer.Models
+    {
+    public class PaginatedProductViewModel
+    {
+        public List<Product> Items { get; set; } = new();
+        public int TotalCount { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages => (int)System.Math.Ceiling(TotalCount / (double)PageSize);
+
+        // overall counts (over full result set, not just the page)
+        public int OnStockCount { get; set; }
+        public int LowCount { get; set; }
+        public int OutCount { get; set; }
+    }
+}
