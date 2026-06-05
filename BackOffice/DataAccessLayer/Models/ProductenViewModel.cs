456f983d76dadc636c001398d5c8b71dcf37624e
BeetleBeast <78571657+BeetleBeast@users.noreply.github.com>
2026-06-05 15:05:35 +0200

    added bulkEdit and productenviewmodel from xavier

---
+++ BackOffice/DataAccessLayer/Models/ProductenViewModel.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace BackOffice.DataAccessLayer.Models
+{
+    public class ProductenViewModel
+    {
+        [Required]
+        public List<Product> Producten { get; set; }
+        public Product? ProductEditForm { get; set; }
+        public BulkEdit? BulkEdit { get; set; }
+
+        public ProductenViewModel() { }
+
+        public ProductenViewModel(List<Product> producten, Product? productEditForm = null, BulkEdit? bulkEdit = null)
+        {
+            Producten = producten;
+            ProductEditForm = productEditForm;
+            BulkEdit = bulkEdit;
+        }
+    }
+
+}
