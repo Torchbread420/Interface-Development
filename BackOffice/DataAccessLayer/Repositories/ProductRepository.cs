c0f57e33e96f49d5d98021b2ca961577b569fdae
BeetleBeast <78571657+BeetleBeast@users.noreply.github.com>
2026-06-05 15:17:52 +0200

    added methods to repositoey and fxed some path problems

--- BackOffice/DataAccessLayer/Repositories/ProductRepository.cs
+++ BackOffice/DataAccessLayer/Repositories/ProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using DataAccessLayer.Interfaces;
+﻿using BackOffice.DataAccessLayer.Models;
+using DataAccessLayer.Interfaces;
 using DataAccessLayer.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -13,16 +14,35 @@ namespace DataAccessLayer.Repositories
     {
         private readonly MatrixIncDbContext _context;
 
-        public ProductRepository(MatrixIncDbContext context) 
+        public ProductRepository(MatrixIncDbContext context)
         {
             _context = context;
         }
+        // add products
         public void AddProduct(Product product)
         {
             _context.Products.Add(product);
             _context.SaveChanges();
         }
-
+        // Delete products
+        public void DeleteProductsById(List<int> productIds)
+        {
+            var products = _context.Products.Where(p => productIds.Contains(p.Id)).ToList();
+            if (products.Any())
+            {
+                _context.Products.RemoveRange(products);
+                _context.SaveChanges();
+            }
+        }
+        public void DeleteProductById(int productId)
+        {
+            var product = _context.Products.Find(productId);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+            }
+        }
         public void DeleteProduct(Product product)
         {
             _context.Products.Remove(product);
@@ -38,11 +58,40 @@ namespace DataAccessLayer.Repositories
         {
             return _context.Products.Include(p => p.Parts).FirstOrDefault(p => p.Id == id);
         }
+        public List<Product> GetProductsByIds(List<int> productIds)
+        {
+            List<Product> products = [];
+            foreach (var id in productIds)
+            {
+                products.Add(GetProductById(id));
+            }
+            return products;
+        }
 
         public void UpdateProduct(Product product)
         {
             _context.Products.Update(product);
             _context.SaveChanges();
+        }
+        public void UpdateProducts(List<Product> products)
+        {
+            foreach (var product in products)
+            {
+                _context.Products.Update(product);
+            }
+            _context.SaveChanges();
+        }
+        public void EditProducts(BulkEdit bulkEdit)
+        {
+            List<int> ints = bulkEdit.ProductIds;
+            List<Product> producten = this.GetProductsByIds(ints);
+            foreach (var item in producten)
+            {
+                item.KostPrice = bulkEdit.KostPrice;
+                item.SalePercentage = bulkEdit.SalePercentage;
+            }
+
+            this.UpdateProducts(producten);
         }
     }
 }
